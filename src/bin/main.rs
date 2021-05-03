ab8bcddb73deca21fedae810855efb8d9e52fe09
yuki-chai <chaiyujin@gmail.com>
2021-05-03 16:57:15 +0800

    multi-threaded server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,20 @@
 use std::fs;
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use multi_thread_web_server_rsbook::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,8 +24,14 @@ fn handle_connection(mut stream: TcpStream) {
 
     // b"": byte string
     let get = b"GET / HTTP/1.1\r\n";
+    // simulate a slow request
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        // simulate a slow request
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use std::thread;
+use std::sync::{mpsc, Mutex, Arc};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            // create some threads and store them in the vector
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            // MutexGuard from lock() is dropped here, because any temporary
+            // values used in the expression on the right hand side of
+            // the equals sign are immediately dropped when the let statement ends.
+            // Thus, this thread won't block others to receive a new job.
+
+            println!("Worker {} got a job; executing.", id);
+
+            job();
+            // job is done, loop
+        });
+        Worker { id, thread }
+    }
+}
