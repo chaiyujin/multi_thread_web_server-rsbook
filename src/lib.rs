3aae3645c2a5db000cbca89bd9e206982bd50225
yuki-chai <chaiyujin@gmail.com>
2021-05-03 17:10:43 +0800

    graceful shutdown and cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,14 @@ use std::sync::{mpsc, Mutex, Arc};
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -29,29 +34,55 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers by join().");
+        for worker in &mut self.workers {
+            println!("> Shutting down worker {}", worker.id);
+            // join() need to take ownership, which is given by Option.take()
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        // move the receiver into closure
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let msg = receiver.lock().unwrap().recv().unwrap();
             // MutexGuard from lock() is dropped here, because any temporary
             // values used in the expression on the right hand side of
             // the equals sign are immediately dropped when the let statement ends.
             // Thus, this thread won't block others to receive a new job.
 
-            println!("Worker {} got a job; executing.", id);
-
-            job();
-            // job is done, loop
+            match msg {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate", id);
+                    break;
+                }
+            }
         });
-        Worker { id, thread }
+
+        Worker { id, thread: Some(thread) }
     }
 }
